f35d40f43252489b8a7d8b55a7e8ff3ef9e63f73
Stephy Miehle <stephy@blindingstars.com>
2019-09-20 19:42:01 -0500

    Remove dead imports

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 
-import { Container, Row, Col } from "react-bootstrap"
+import { Container } from "react-bootstrap"
 
 import Header from "./header"
 import Navbar from "./navBar"
