3910e081c226845109433616c8d199e08a88f5ef
Stephy Miehle <stephy@blindingstars.com>
2019-09-20 22:14:34 -0500

    Temp. limit width on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,30 @@
 import React from "react"
 
+import { Row, Col } from "react-bootstrap"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout pageInfo={{ pageName: "index" }}>
     <SEO title="Home" keywords={[`gatsby`, `react`, `bootstrap`]} />
-    <blockquote className="pullquote">
-      The art challenges the technology, and the technology inspires the art.
-      <cite className="d-block">&mdash;&thinsp;John Lasseter</cite>
-    </blockquote>
-    <p>
-      MCC has a{" "}
-      <a href="https://3dpotter.com/printers/potterbot-9-pro">
-        3D PotterBot 9 Pro
-      </a>{" "}
-      in the Art department, and this site will help you learn how to use it.
-      Check back soon for links to resources and software.
-    </p>
+    <Row>
+      <Col lg="8" className="mx-auto">
+        <blockquote className="pullquote">
+          The art challenges the technology, and the technology inspires the
+          art.
+          <cite className="d-block">&mdash;&thinsp;John Lasseter</cite>
+        </blockquote>
+        <p>
+          MCC has a{" "}
+          <a href="https://3dpotter.com/printers/potterbot-9-pro">
+            3D PotterBot 9 Pro
+          </a>{" "}
+          in the Art department, and this site will help you learn how to use
+          it. Check back soon for links to resources and software.
+        </p>
+      </Col>
+    </Row>
   </Layout>
 )
 
